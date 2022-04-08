343065bc580c5b9d4e880a5598b80ff57e6e6776
Andreea Sburlea <andreea.sburlea@fortech.ro>
2022-04-08 14:43:53 +0300

    Chapter3 - hello world app

---
+++ Chapter3/Chapter3/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Chapter3
+//
+//  Created by Andreea Sburlea on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func showMessage(sender: UIButton){
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
